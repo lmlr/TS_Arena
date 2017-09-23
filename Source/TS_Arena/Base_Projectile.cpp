c94ea5d6ec63c7f029e2225b7a7b99252372fbc8
lmlr <leo.mueller@rwth-aachen.de>
2017-09-23 21:47:11 +0200

    Added Base Projectile

---
+++ Source/TS_Arena/Base_Projectile.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Base_Projectile.h"
+#include "Classes/Components/SphereComponent.h"
+#include "Classes/GameFramework/ProjectileMovementComponent.h"
+#include "Classes/Components/StaticMeshComponent.h"
+
+
+// Sets default values
+ABase_Projectile::ABase_Projectile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	bReplicates = true;
+	bReplicateMovement = true;
+
+	CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("Collision Sphere"));
+	RootComponent = CollisionSphere;
+
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement Component"));
+	ProjectileMovement->SetUpdatedComponent(CollisionSphere);
+
+	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Projectile Mesh"));
+	ProjectileMesh->SetupAttachment(CollisionSphere);
+}
+
+// Called when the game starts or when spawned
+void ABase_Projectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABase_Projectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/TS_Arena/Base_Projectile.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Base_Projectile.generated.h"
+
+UCLASS()
+class TS_ARENA_API ABase_Projectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABase_Projectile();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Projectile",
+		meta = (AllowPrivateAccess = "true"))
+	class USphereComponent* CollisionSphere;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Projectile",
+		meta = (AllowPrivateAccess = "true"))
+	class UProjectileMovementComponent* ProjectileMovement;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Projectile",
+		meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* ProjectileMesh;
+	
+
+	
+	
+};
